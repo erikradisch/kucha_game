21e718ee87d500b111bd594092bbc81198cae9e2
Erik Radisch <radisch@saw-leipzig.de>
2024-12-07 11:57:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Game></Game>
+  </v-app>
+</template>
+
+<script>
+import Game from "./components/Game.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Game,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
